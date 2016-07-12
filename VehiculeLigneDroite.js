9776b7c3147a924cc5831321d7b0ff279ba86113
samgrat <smail.bamba@gmail.com>
2016-07-12 17:16:05 +0200

    12/07

---
+++ VehiculeLigneDroite.js
@@ -0,0 +1,125 @@
+//variables :
+	// 1 m = 10 px
+	var WIDTH = 1010;            // OF SCREEN IN PIXELS
+	var HEIGHT = 500;            // OF SCREEN IN PIXELS
+	var BALLS = 1;               // IN SIMULATION
+	var WALL = 5;                // FROM SIDE IN PIXELS
+	//var WALL_FORCE = 400;        // ACCELERATION PER MOVE
+	//var SPEED_LIMIT = 30;        // FOR ball VELOCITY
+	var SPEED = 1;              // m  / s : vitesse de départ
+	var StartSPEED = SPEED;
+	var ACCELERATION = 1.39;     // m / s²
+	var DECELERATION = 0;
+	var BALL_RADIUS = 10;        // FOR ballS IN PIXELS
+	var SPEED_LIMIT = 40;         // km / h
+	//var OFFSET_START = 20;       // FROM WALL IN PIXELS
+	//var FRAMES_PER_SEC = 40;     // SCREEN UPDATE RATE
+	var END = new Boolean(false);             // Fin de l'animation ?
+	var FRICTION = 0.8;          // Coefficient cinétique de friction
+	var MASS = 1500;             // Kg Poids du vehicule
+	var flag = 0;                // critère d'arret
+	var x = 0;                   // position du véhicule
+	var DFA = 0;                   // distance de freinage atteinte ?
+	
+	var i = {x:x+BALL_RADIUS*2,y:HEIGHT/2,rayon:BALL_RADIUS}
+	console.log(i)
+
+///////////////////////////////////////////////////////////////////////////////////////
+
+function accelerate(){
+    if (SPEED + ACCELERATION*0.02 <= SPEED_LIMIT/3.6) {  // si le véhicule ne va pas dépasser la limite de vitesse (km/h vers m/s: /3.6)
+		SPEED += ACCELERATION*0.02}						// la boucle s'actualise toute les 20 ms d'où le *0.02
+    else if (SPEED > SPEED_LIMIT/3.6){            		// si le véhicule a dépassé la vitesse limite il freine
+		SPEED -= ACCELERATION*0.02}						// ralentir
+}// fin accelerate()
+
+function decelerate(){
+    if (DFA == 0){ 		//Pour n'affecter a Decceleration une valeur une seule fois
+        DECELERATION = (SPEED*SPEED)/(2*((WIDTH-BALL_RADIUS-x)/10))      // a = v²/2x
+        DFA = 1
+		SPEED -= DECELERATION*0.02}
+    else{
+		SPEED -= DECELERATION*0.02}
+}// fin decelerate()
+
+function stop_it(){
+    // arret de l'animation
+    flag =0
+}// fin stop_it()
+
+function start_it(){
+    // démarrage de l'animation
+    if (END == true){
+        i.x = 0
+		END = false}
+    if (flag == 0){       // pour ne lancer qu'une seule boucle
+        flag =1
+		moveCircle()
+		}
+}//fin start_it()
+
+function moveCircle(){
+    if (SPEED != 0){                // si le demarrage ne se fait pas à vitesse nulle
+
+        //if    distance au mur      >      distance de freinage       + Distance de sécurité    :
+        if ((WIDTH-BALL_RADIUS-i.x)/10 > ((Math.pow(SPEED,2)/(2*FRICTION*9.81))+BALL_RADIUS/10 && DFA == 0)){
+			accelerate()} //accélérer
+        else{
+			decelerate()} //ralentir
+	}
+    else{
+        console.log("Speed 1 ",SPEED)
+		accelerate()}
+
+    if (i.x<WIDTH-BALL_RADIUS && SPEED > 0){ // si la voiture n'a pas atteint la fin et n'est pas arretée
+		i.x+=SPEED/5}
+    else{
+        END = true
+        SPEED = StartSPEED
+        DFA= 0
+		stop_it()}
+		draw()
+
+}// fin de moveCircle
+
+function draw(){
+	var canvas = document.getElementById('mon_canvas');
+    var context = canvas.getContext('2d');
+	
+		// on "efface" l'ancien cercle
+	context.beginPath();
+	context.arc(i.x+BALL_RADIUS*2, HEIGHT/2, BALL_RADIUS*2, 0, Math.PI*2);
+	context.fillStyle = "white";
+	context.fill();
+	context.closePath();
+	
+	console.log("translating... ","x= ",i.x)
+	i.x =+ BALL_RADIUS
+	console.log("x2= ",i.x)
+	
+		// on le redessine
+	context.beginPath();
+	context.arc(i.x+BALL_RADIUS*2, HEIGHT/2, BALL_RADIUS, 0, Math.PI*2);
+	context.fillStyle = "red";
+	context.fill();
+	context.closePath();
+	console.log("translated LUL")
+	
+	if (flag >0){
+		setTimeout(moveCircle(), 10000);}
+}// fin de draw()
+
+function init(){
+
+    var canvas = document.getElementById('mon_canvas');
+    var context = canvas.getContext('2d');
+ 
+	
+		//cercle du vehicule
+	context.beginPath();
+	context.arc(BALL_RADIUS*2, HEIGHT/2, BALL_RADIUS, 0, Math.PI*2);
+	context.fillStyle = "red";
+	context.fill();
+	context.closePath();
+	alert("did it dad");
+	}// fin de draw()
